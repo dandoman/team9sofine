25ac3ddf083bd9e0d4a0885f694306d122d97106
Davide Cannucci <dcannucci@groupon.com>
2016-04-30 17:37:53 -0700

    Set up thymeleaf templating

--- src/main/java/com/kink/mvc/ExternalPageController.java
+++ src/main/java/com/kink/mvc/ExternalPageController.java
@@ -6,4 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ExternalPageController {
 
+	@RequestMapping("/test")
+	public String index() {
+		return "index";
+	}
 }
