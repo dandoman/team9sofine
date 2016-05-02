19c025d147038f200398ff53b127093087a4b098
Davide Cannucci <dcannucci@groupon.com>
2016-05-01 21:38:27 -0700

    Fixed up more templating stuff

--- src/main/java/com/kink/mvc/ExternalPageController.java
+++ src/main/java/com/kink/mvc/ExternalPageController.java
@@ -1,13 +1,16 @@
 package com.kink.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ExternalPageController {
 
-	@RequestMapping("/test")
-	public String index() {
+	@RequestMapping("/index.html")
+	public String index(Model model) {
+		String name = "Daveeee";
+		model.addAttribute("name", name);
 		return "index";
 	}
 }
